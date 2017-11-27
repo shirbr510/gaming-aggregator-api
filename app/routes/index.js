48645d3d089994ccf0a45ef3131f2e0c575273a8
Shir Brass <shirbr510@gmail.com>
2017-11-27 19:58:26 +0200

    implemented user info retrieval mechanism

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,13 @@
 // @flow
 import users from './users'
 import auth from './auth'
+import * as steamWorker from '../workers/SteamWorker';
 
 import express from "express";
 const router = express.Router();
 
+router.get('/games-worker-test',steamWorker.runGamesWorker)
+router.get('/user-worker-test',steamWorker.runUserWorker)
 router.use('/auth',auth);
 router.use('/users',users);
 
--- app/workers/SteamWorker.js
+++ app/workers/SteamWorker.js
@@ -3,13 +3,10 @@ import * as _ from 'lodash';
 import * as usersProvider from '../database/users'
 import * as steamGames from '../database/steamGames'
 import PlayerService from '../services/SteamPlayerService'
+import UserService from '../services/SteamUserService'
 const {STEAM_API_KEY} = process.env;
 const playerService = new PlayerService();
-
-const getUserGames=async (steamUserId)=>{
-    const gamesObject=await playerService.getOwnedGames(steamUserId,true,true);
-    return gamesObject.games;
-}
+const userService = new UserService();
 
 export const runGamesWorker= async()=>{
     const users=await usersProvider.getUsers();
@@ -17,8 +14,20 @@ export const runGamesWorker= async()=>{
         if(user.platforms){
             const{platforms:{steam}}=user;
             const {platformId} = steam;
-            const games = await getUserGames(platformId);
+            const {games} = await playerService.getOwnedGames(platformId,true,true);
             steamGames.setUserSteamGames(user.id,games);
+        }
+    })
+}
+
+export const runUserWorker= async()=>{
+    const users=await usersProvider.getUsers();
+    _.each(users,async user=>{
+        if(user.platforms){
+            const{platforms:{steam}}=user;
+            const {platformId} = steam;
+            const userInfo = await userService.getPlayerSummary(platformId);
+            //TODO: Do something with user's info
         }
     })
 }
